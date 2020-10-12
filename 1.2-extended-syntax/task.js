798676d3dce2c0f5b50d8c0306607cd1f95054aa
AlexanderAndreevGIT <andreev-alex@inbox.ru>
2020-10-12 13:04:52 +0300

    first commit extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,43 @@
+"use strict";
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let x;
+    let D =  b ** 2 - 4 * a * c;
+    if (D < 0) {
+    	x = [];
+    } else if (D == 0) {
+    	x = [-b / (2 * a)];
+    } else {
+    	x = [(-b + D ** (1/2)) / (2 * a), (-b - D ** (1/2)) / (2 * a)];
+    }
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let averageMark;
+    let sumMarks = 0;
+    if (marks.length > 5) {
+    	marks = marks.slice(0,5);
+    	console.log('количество оценок больше 5');
+    }
+    if (marks.length == 0) {
+    	averageMark = 0;
+    } else {
+    	for (let i = 0; i < marks.length; i++) {
+    		sumMarks += marks[i];
+    	}
+    	averageMark = sumMarks / marks.length;
+    }
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+	 let result;
+    const currentYear = new Date().getFullYear();
+    if ((currentYear - dateOfBirthday.getFullYear()) >= 18) {
+    	result = `Не желаете ли олд-фэшн, ${name}?`
+    } else {
+    	result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`
+    }
+    return result;
 }
