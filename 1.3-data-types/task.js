39d02cae6e09369b6ffb72b8c3ba7c8f2383d787
AlexanderAndreevGIT <andreev-alex@inbox.ru>
2020-10-18 16:44:23 +0300

    first commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,38 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+   
+    let result;
+
+    if (isNaN(percent) === true) {
+    	result = `Параметр "Процентная ставка" содержит неправильное значение`;
+    	return result;
+    }
+    if (isNaN(contribution) === true) {
+    	result = `Параметр "Начальный взнос" содержит неправильное значение`;
+    	return result;
+    }
+    if (isNaN(amount) === true) {
+    	result = `Параметр "Общая стоимость" содержит неправильное значение`;
+    	return result;
+    }
+
+    let mortgage = amount - contribution;
+    let numberOfMonth = (date.getMonth() + date.getFullYear() * 12) - (new Date().getMonth() + new Date().getFullYear() * 12);
+    let P = percent / 100 / 12;
+    let monthlyPayment = mortgage * (P + P / (((1 + P) ** numberOfMonth) - 1));
+    let totalAmount = monthlyPayment * numberOfMonth;
+    console.log(totalAmount.toFixed(2));
+    console.log(typeof totalAmount);
+    return totalAmount.toFixed(2);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+	let greeting;
+	if (name === false || name === undefined || name === "") {
+		greeting = 'Привет, мир! Меня зовут Аноним.'
+	} else {
+		greeting = `Привет, мир! Меня зовут ${name}.`
+	}
+    return greeting;
 }
