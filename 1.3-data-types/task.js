ba98b10d1a5f299b0fe023345ff28bcb9d782a59
AlexanderAndreevGIT <andreev-alex@inbox.ru>
2020-10-22 17:18:19 +0300

    reworked data types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -19,20 +19,22 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
     let mortgage = amount - contribution;
     let numberOfMonth = (date.getMonth() + date.getFullYear() * 12) - (new Date().getMonth() + new Date().getFullYear() * 12);
-    let P = percent / 100 / 12;
+    const P = percent / 100 / 12;
     let monthlyPayment = mortgage * (P + P / (((1 + P) ** numberOfMonth) - 1));
     let totalAmount = monthlyPayment * numberOfMonth;
-    console.log(totalAmount.toFixed(2));
+    console.log(Number(totalAmount.toFixed(2))) ;
     console.log(typeof totalAmount);
-    return totalAmount.toFixed(2);
+    return Number(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
 	let greeting;
-	if (name === false || name === undefined || name === "") {
-		greeting = 'Привет, мир! Меня зовут Аноним.'
+	if (!!name === false || name === 'null' || name === 'undefined') {
+		greeting = 'Привет, мир! Меня зовут Аноним'
 	} else {
-		greeting = `Привет, мир! Меня зовут ${name}.`
+		greeting = `Привет, мир! Меня зовут ${name}`
 	}
+    
+    console.log(greeting)
     return greeting;
 }
